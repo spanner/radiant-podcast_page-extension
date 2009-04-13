109d0f37dcd462a887844d19c33b7675e3029ac7
will <will@spanner.org>
2009-04-13 16:39:11 +0100

    layout mime type fixed in migration

--- db/migrate/001_podcast_context.rb
+++ db/migrate/001_podcast_context.rb
@@ -1,7 +1,7 @@
-class PodcastBits < ActiveRecord::Migration
+class PodcastContext < ActiveRecord::Migration
 
   def self.up
-    Layout.create :name => 'podcast', :content_type => 'application/xml+rss', :content => "<r:content />" unless Layout.find_by_name('podcast')
+    Layout.create :name => 'Podcast', :content_type => 'application/rss+xml', :content => "<r:content />" unless Layout.find_by_name('Podcast')
     Radiant::Config['assets.additional_thumbnails'] = Radiant::Config['assets.additional_thumbnails'] + ", itunes=1024x1024#" unless Radiant::Config['assets.additional_thumbnails'] =~ /itunes/
   end
 
